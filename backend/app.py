1fd922e339d19663f7b2adeb2399a7a7982980be
Sebastian Montoya <sebastianmontoya@Caterpie.local>
2025-08-25 00:45:20 +1000

    setup my frontend and also setup some more basic flask endpoints, such as endpoints for the homepage, about page, and the projects page

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def homepage():
     return "This is the homepage"
 
+@app.route("/projects")
+def projects():
+    return "This is the projects page"
+
+@app.route("/about")
+def about():
+    return "This is the about page"
 
 if __name__ == "__main__":
     app.run(debug=True)
